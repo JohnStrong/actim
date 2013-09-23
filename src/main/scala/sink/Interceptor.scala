d3916920e67c10a9a1d5078622904cf6c65a26b7
John Strong <strngj411@gmail.com>
2013-09-23 14:12:55 +0100

    better xml parsing in Interceptor class

--- src/main/scala/sink/Interceptor.scala
+++ src/main/scala/sink/Interceptor.scala
@@ -1,14 +1,15 @@
 package chatclient.sink
 
 import akka.actor.{ Actor, Props}
+import xml._
 
 object Interceptor {
 
-	case class Account(message:xml.Elem)
+	case class Account(message: Elem)
 
-	case class Message(message: xml.Elem)
+	case class Message(message: Elem)
 
-	case class Done(status:String)
+	case class Done(status: String)
 
 	/**
 	* Remote Actor that handles incoming messages from any client and responds with an xml message
@@ -23,7 +24,11 @@ object Interceptor {
 		val clientEntity = context.actorOf(Props(classOf[ClientEntity], 
 			clientStore), name = "cliententity.store.chatclient")
 
-		override def preStart():Unit = {
+		// all client accounts
+		val accounts = allAccounts()
+
+		// retrieve all accounts in datastore
+		def allAccounts() {
 			val users = clientEntity ! All()
 			println(users)
 		}
@@ -32,8 +37,14 @@ object Interceptor {
 		def receive = {
 
 			// if successful return profile details
-			case Account(login) => println(login) //todo
-			case Message(message) => println(message) //todo
+			case Account(login) =>{ 
+				login \ "login" match {
+					case <login>{email}</login> => println(email)
+					case _ => println("unrecognised message")
+				}
+			}
+
+			case Message(message) => println(message \\ "to"); println(message \\ "from") //todo
 			case Done(x) => context.stop(self)
 			case _ => println("unrecognised message")
 		}
